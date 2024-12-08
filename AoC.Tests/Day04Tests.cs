b03c77298da864cf3908b51334be7b36074659a7
Rocco Romeo <rromeo@live.ca>
2024-12-07 19:18:40 -0500

    - Add Day04 project - Add failing Test04Tests class

---
+++ AoC.Tests/Day04Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day04;
+
+
+namespace AoC.Tests
+{
+    [TestClass]
+    public class Day04Tests
+    {
+        static string sampleData1 =
+@"
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+";
+
+        string sampleData2 =
+@"
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+";
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.IsTrue(Day04.Program.CountOccurances(sampleData1, "XMAS") == 18);
+            Assert.IsTrue(Day04.Program.CountOccurances(sampleData2, "XMAS") == 18);
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day04;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    public static int CountOccurances(string data, string word)
+    {
+        throw new NotImplementedException();
+
+    }
+}
