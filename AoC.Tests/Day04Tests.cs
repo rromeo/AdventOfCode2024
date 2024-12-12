7c1c69e7a90e7887092113b3ec6f5df422bf010c
Rocco Romeo <rromeo@live.ca>
2024-12-12 18:01:06 -0500

    - Added unit tests for GetVerticals, GetRtlDiagonals, and GetLtrDiagonals - Added code to solve the problem - Added input file - Passed Part 1

--- AoC.Tests/Day04Tests.cs
+++ AoC.Tests/Day04Tests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Day04;
+using System.IO;
 
 
 namespace AoC.Tests
@@ -13,7 +14,9 @@ namespace AoC.Tests
     [TestClass]
     public class Day04Tests
     {
-        static string sampleData1 =
+        static readonly string[] diagTest = ["abc", "def", "ghi"];
+
+        const string sampleData1 =
 @"
 MMMSXXMASM
 MSAMXMSMSA
@@ -27,7 +30,7 @@ MAMMMXMMMM
 MXMXAXMASX
 ";
 
-        string sampleData2 =
+        const string sampleData2 =
 @"
 ....XXMAS.
 .SAMXMS...
@@ -41,11 +44,59 @@ S.S.S.S.SS
 .X.X.XMASX
 ";
 
+        string[] ParseData(string data)
+        {
+            var lines = new List<string>();
+            using (var reader = new StringReader(data))
+            {
+                string? line;
+                while ( (line = reader.ReadLine()) != null) 
+                { 
+                    if (string.IsNullOrEmpty(line)) 
+                        continue;
+                    lines.Add(line);
+                }
+
+            }
+
+            return lines.ToArray();
+        }
+
+        [TestMethod] 
+        public void TestVerticalLines() 
+        {
+            string[] expecting = ["adg", "beh", "cfi"];
+            var diagonals = Day04.Program.GetVerticalLines(diagTest).ToArray();
+
+            CollectionAssert.AreEqual(expecting, diagonals);
+        }
+
+        [TestMethod]
+        public void TestLtrDiagonal() 
+        {
+            string[] expecting = ["g", "dh", "aei", "bf", "c"];
+            var diagonals = Day04.Program.GetLtrDiagonalLines(diagTest).ToArray();
+
+            CollectionAssert.AreEqual(expecting, diagonals);
+        }
+
+        [TestMethod]
+        public void TestRtlDiagonal()
+        {
+            string[] expecting = ["a","bd","ceg", "fh", "i"];
+            var diagonals = Day04.Program.GetRtlDiagonalLines(diagTest).ToArray();
+
+            CollectionAssert.AreEqual(expecting, diagonals);
+        }
+
+
+
+
         [TestMethod]
         public void Part1()
         {
-            Assert.IsTrue(Day04.Program.CountOccurances(sampleData1, "XMAS") == 18);
-            Assert.IsTrue(Day04.Program.CountOccurances(sampleData2, "XMAS") == 18);
+            Assert.IsTrue(Day04.Program.CountOccurences(ParseData(sampleData1), "XMAS") == 18);
+            Assert.IsTrue(Day04.Program.CountOccurences(ParseData(sampleData2), "XMAS") == 18);
         }
     }
 }
--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,15 +1,169 @@
-﻿namespace Day04;
+﻿
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Day04;
 
 public class Program
 {
-    static void Main(string[] args)
+    static async Task<int> Main(string[] args)
     {
+        try
+        {
+            var data = await File.ReadAllLinesAsync("input.txt");
 
+            Debug.Assert(IsSquare(data));
+
+            var word = "XMAS";
+            var count = CountOccurences(data, word);
+            Console.WriteLine($"There are {count} occurences of '{word}' in the input");
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+            return ex.HResult;
+        }
     }
 
-    public static int CountOccurances(string data, string word)
+    static bool IsSquare(string[] lines)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if ( lines[i].Length != lines[i].Length )
+                return false;
+        } 
+
+        return true;
+    }
+
+    public static IEnumerable<string> GetVerticalLines(string[] lines)
+    {
+        var columns = lines[0].Length;
+
+        for (int column = 0; column < columns; column++)
+        {
+            char[] c = new char[columns];
+            for(int i = 0; i< lines.Length; i++)
+            {
+                c[i] = lines[i][column];
+            }
+
+            yield return new string(c);
+
+        }
+    }
+
+    public static IEnumerable<string> GetLtrDiagonalLines(string[] lines)
+    {
+        var size = lines.Length;
+
+        for(int row = lines.Length-1; row > 0; row--)
+        {
+            char[] c = new char[size - row];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = lines[row+i][i];
+            }
+
+            yield return new string(c);
+        }
+
+
+        for (int column = 0; column < size; column++)
+        {
+            char[] c = new char[size-column];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = lines[i][column+i];
+            }
+
+            yield return new string(c);
+
+        }
+    }
+
+    public static IEnumerable<string> GetRtlDiagonalLines(string[] lines)
+    {
+        var size = lines.Length;
+
+        for (int column = 0; column < size; column++)
+        {
+            char[] c = new char[column +1];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = lines[i][column-i];
+            }
+
+            yield return new string(c);
+        }
+
+
+        for (int row = 1; row < size; row++)
+        {
+            char[] c = new char[size - row];
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i] = lines[row+i][size-i-1];
+            }
+
+            yield return new string(c);
+
+        }
+    }
+
+    private static int CountOccurences(string line, string word)
+    {
+        int count = 0;
+        int startIndex = 0;
+        int index;
+        while ( (index = line.IndexOf(word, startIndex,StringComparison.Ordinal )) != -1) 
+        {
+            count++;
+            startIndex = index + 1;
+        }
+
+        return count;
+    }
+
+    public static int CountOccurences(string[] lines, string word)
+    {
+        var wordRev = new string(word.Reverse().ToArray());
+        int count = 0;
+
+        // Horizontal
+        foreach (string line in lines) 
+        {
+            count += CountOccurences(line, word);
+            count += CountOccurences(line, wordRev);
+        }
+
+        // Vertical
+        foreach (string line in GetVerticalLines(lines))
+        {
+            count += CountOccurences(line, word);
+            count += CountOccurences(line, wordRev);
+        }
+
+        // Diagonal - left to right
+        foreach (string line in GetLtrDiagonalLines(lines))
+        {
+            count += CountOccurences(line, word);
+            count += CountOccurences(line, wordRev);
+        }
+
+        // Diagonal - left to right
+        foreach (string line in GetRtlDiagonalLines(lines))
+        {
+            count += CountOccurences(line, word);
+            count += CountOccurences(line, wordRev);
+        }
+
+
+        return count;
+
 
     }
 }
